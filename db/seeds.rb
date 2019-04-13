117610a254e19a3fc3deba04ec518fc1a39a810d
Eric Pickup <pickup@uwindsor.ca>
2019-04-13 17:40:12 -0400

    Fixed seeding and major reference

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ NUMBER_OF_USERS_TO_GENERATE.times do
 end
 
 User.all.each do |user|
-  (User.first.id..(User.last.id + NUMBER_OF_USERS_TO_GENERATE)).to_a.sample(NUMBER_OF_RANDOM_FRIENDS_PER_USER).each do |random_user_id|
+  (User.first.id..User.last.id).to_a.sample(NUMBER_OF_RANDOM_FRIENDS_PER_USER).each do |random_user_id|
     random_user = User.find(random_user_id)
     next if random_user_id == user.id || (user.friends.include? random_user)
     user.friends << random_user
