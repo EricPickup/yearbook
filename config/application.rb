cd230717e647682734862bba7165f67c6062d1f5
Eric Pickup <pickup@uwindsor.ca>
2019-04-13 17:11:13 -0400

    Created seperate google API project for Yearbook

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Yearbook
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.middleware.use OmniAuth::Builder do
-      provider :google_oauth2, '46281716787-5jfh8nvo413qkge6gm2p5rd03rsrpc6e.apps.googleusercontent.com', 'ec9ZO-yN2hhEVd6kNeSNuCdK', { hd: 'uwindsor.ca', prompt: 'none' }
+      provider :google_oauth2, '157630002807-n0q3bmea7dmq5s4fbnt3fuvigfhjaj24.apps.googleusercontent.com', '2hVQxd9W3Dj1J3AB4J_sPwQr', { hd: 'uwindsor.ca' }
     end
   end
 end
