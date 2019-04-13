241469adcbc4ab3780d60da7ba894fc361bd662e
Eric Pickup <pickup@uwindsor.ca>
2019-04-13 16:33:28 -0400

    Changed database from sqlite3 to postgresql

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     current_user ||= User.find(session[:user_id]) if session[:user_id]
+
+  rescue ActiveRecord::RecordNotFound
+    reset_session
+    redirect_to root_url
   end
 
   def logged_in?
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-NUMBER_OF_USERS_TO_GENERATE = 100
-NUMBER_OF_RANDOM_FRIENDS_PER_USER = 20
+# NUMBER_OF_USERS_TO_GENERATE = 100
+# NUMBER_OF_RANDOM_FRIENDS_PER_USER = 20
 
-NUMBER_OF_USERS_TO_GENERATE.times do
-  User.new(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    college: Faker::University.name,
-    relationship: rand(1..2) == 1 ? "Single" : "In a relationship",
-    location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}"
-    ).save
-end
+# NUMBER_OF_USERS_TO_GENERATE.times do
+#   User.new(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     college: Faker::University.name,
+#     relationship: rand(1..2) == 1 ? "Single" : "In a relationship",
+#     location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}"
+#     ).save
+# end
 
-User.all.each do |user|
-  (User.first.id..(User.last.id + NUMBER_OF_USERS_TO_GENERATE)).to_a.sample(NUMBER_OF_RANDOM_FRIENDS_PER_USER).each do |random_user_id|
-    random_user = User.find(random_user_id)
-    next if random_user_id == user.id || (user.friends.include? random_user)
-    user.friends << random_user
-  end
-end
+# User.all.each do |user|
+#   (User.first.id..(User.last.id + NUMBER_OF_USERS_TO_GENERATE)).to_a.sample(NUMBER_OF_RANDOM_FRIENDS_PER_USER).each do |random_user_id|
+#     random_user = User.find(random_user_id)
+#     next if random_user_id == user.id || (user.friends.include? random_user)
+#     user.friends << random_user
+#   end
+# end
