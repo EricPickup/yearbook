097fe2b8f8c3b1b7901732b99ba39d07a3b5a14e
Eric Pickup <pickup@uwindsor.ca>
2019-04-13 18:40:57 -0400

    Each new user will have 15 random friends for demo purposes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,15 @@
 class SessionsController < ApplicationController
+  NUMBER_OF_INITIAL_RANDOM_FRIENDS_PER_USER = 15
+
   def create
     user = User.find_or_create_by(:email => auth_hash[:info][:email]) do |user|
       user.email = auth_hash[:info][:email]
       user.name = auth_hash[:info][:name]
+      (User.first.id..User.last.id).to_a.sample(NUMBER_OF_INITIAL_RANDOM_FRIENDS_PER_USER).each do |random_user_id|
+        random_user = User.find(random_user_id)
+        next if random_user_id == user.id || (user.friends.include? random_user)
+        user.friends << random_user
+      end
     end
 
      session[:user_id] = user.id
