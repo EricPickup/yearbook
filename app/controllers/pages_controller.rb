04a4d6c6b5bc9eb9be73520aa3aee96a08addbb5
Eric Pickup <pickup@uwindsor.ca>
2019-04-13 00:29:32 -0400

    Added delete post option, moved post list to one partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
     if current_user.nil?
       redirect_to root_url, alert: "You need to be signed in to view your profile!"
     else
-      @latest_posts = current_user.posts.last(10).reverse
+      @posts = current_user.posts.last(10).reverse
       @post = Post.new
     end
   end
@@ -18,7 +18,7 @@ class PagesController < ApplicationController
   def index
     if current_user
       @post = Post.new
-      @latest_posts = Post.select { |post| post.user == current_user || (post.user.friends.include? current_user) }.last(10).reverse
+      @posts = Post.select { |post| post.user == current_user || (post.user.friends.include? current_user) }.last(10).reverse
     end
   end
 end
