989c42b39b840978839bdfba46194c0f0a775a70
Eric Pickup <pickup@uwindsor.ca>
2019-04-15 18:37:22 -0400

    Remove old API keys (making repo public)

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Yearbook
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.middleware.use OmniAuth::Builder do
-      provider :google_oauth2, '157630002807-n0q3bmea7dmq5s4fbnt3fuvigfhjaj24.apps.googleusercontent.com', '2hVQxd9W3Dj1J3AB4J_sPwQr', { hd: 'uwindsor.ca' }
+      provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], { hd: 'uwindsor.ca' }
     end
   end
 end
